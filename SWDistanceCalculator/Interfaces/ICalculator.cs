93a6f41685dc9ab1a5145011eea3e2dfe34fc085
Lucas Menezes <lucas.forast@gmail.com>
2020-03-12 18:03:51 +0000

    add: calculator

---
+++ SWDistanceCalculator/Interfaces/ICalculator.cs
@@ -0,0 +1,15 @@
+﻿using SWDistanceCalculator.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWDistanceCalculator.Interfaces
+{
+    public interface ICalculator
+    {
+        long CalculateHours(Duration duration);
+        long CalculateStops(long distanceMGLT, long spaceShipMGLT, long hours);
+    }
+}
---
+++ SWDistanceCalculator/Utils/Calculator.cs
@@ -0,0 +1,46 @@
+﻿using SWDistanceCalculator.Interfaces;
+using SWDistanceCalculator.Models;
+using System;
+
+namespace SWDistanceCalculator.Utils
+{
+    /// <summary>
+    /// Calculates hours from duration and how many stops are needed to resupply
+    /// </summary>
+    public class Calculator : ICalculator
+    {
+        private const int DAY_IN_HOURS = 24;
+        private const int WEEK_IN_HOURS = 168;
+        private const int MONTH_IN_HOURS = 730;
+        private const int YEAR_IN_HOURS = 8760;
+
+        public long CalculateHours(Duration duration)
+        {
+            long partialHours;
+            switch (duration.TimeUnit)
+            {
+                case Enums.TimeUnit.Day:
+                    partialHours =  DAY_IN_HOURS;
+                    break;
+                case Enums.TimeUnit.Week:
+                    partialHours = WEEK_IN_HOURS;
+                    break;
+                case Enums.TimeUnit.Month:
+                    partialHours = MONTH_IN_HOURS;
+                    break;
+                case Enums.TimeUnit.Year:
+                    partialHours = YEAR_IN_HOURS;
+                    break;
+                case Enums.TimeUnit.NotValid:
+                default:
+                    throw new ArgumentException(nameof(duration.TimeUnit));
+            }
+            return partialHours * duration.Quantity;
+        }
+
+        public long CalculateStops(long distanceMGLT, long spaceShipMGLT, long hours)
+        {
+            return distanceMGLT / (spaceShipMGLT * hours);
+        }
+    }
+}
